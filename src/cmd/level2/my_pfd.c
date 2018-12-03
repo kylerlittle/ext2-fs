ef5639eb5cfb6a974a614532e1f64984bed3059a
kylerlittle <kyler.little@wsu.edu>
2018-12-02 16:04:22 -0800

    open fix

--- src/cmd/level2/my_pfd.c
+++ src/cmd/level2/my_pfd.c
@@ -20,33 +20,34 @@ void sw_kl_pfd(char *filename)
 
     for(int i=0;i<NOFT;i++) //NOFT is defined as 40
     {
-        openFilePointer=&oft[i];
+        if (running->fd[i] == NULL) break;
+        openFilePointer=running->fd[i];
         if(openFilePointer->refCount==0)
         {
             return;
         }
         printf("%02d",i);
-    }
 
-    if(openFilePointer->mode==0)
-    {
-        printf("RD");
-    }
-    else if(openFilePointer->mode==1)
-    {
-        printf("WR");
-    }
-    else if(openFilePointer->mode==2)
-    {
-        printf("RW");
-    }
-    else if(openFilePointer->mode==3)
-    {
-        printf("AP");
-    }
-    else
-    {
-        printf("--");
+        if(openFilePointer->mode==0)
+        {
+            printf("RD");
+        }
+        else if(openFilePointer->mode==1)
+        {
+            printf("WR");
+        }
+        else if(openFilePointer->mode==2)
+        {
+            printf("RW");
+        }
+        else if(openFilePointer->mode==3)
+        {
+            printf("AP");
+        }
+        else
+        {
+            printf("--");
+        }
     }
 
     printf("\t %6d\t %2d\t %5d\n", openFilePointer->offset, openFilePointer->mptr->dev, openFilePointer->mptr);
